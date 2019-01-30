1753b9ccfc8da5e8f105e3187e78828991b40863
weichao <weichao@yiche.com>
2019-01-31 01:28:04 +0800

    edit FDHitTestInspect

--- FDKit/Classes/Debug/HitTestInspect/FDHitTestInspect.h
+++ FDKit/Classes/Debug/HitTestInspect/FDHitTestInspect.h
@@ -8,7 +8,7 @@
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
-
+//https://www.jianshu.com/p/c294d1bd963d
 @interface FDHitTestInspect : NSObject
 + (void)inspect;
 @end
--- FDKit/Classes/Debug/HitTestInspect/FDHitTestInspect.m
+++ FDKit/Classes/Debug/HitTestInspect/FDHitTestInspect.m
@@ -14,7 +14,7 @@
 + (void)inspect {
     __block NSMutableString *pointInsideChain;
     [UIView aspect_hookSelector:@selector(hitTest:withEvent:) withOptions:AspectPositionInstead usingBlock:^(id<AspectInfo> aspectInfo, CGPoint point ,UIEvent *event) {
-        NSLog(@"hitTest %@", aspectInfo.instance);
+        [self log:aspectInfo];
         UIView * __unsafe_unretained hitTestView;
         NSInvocation *invocation = aspectInfo.originalInvocation;
         [invocation invoke];
@@ -26,7 +26,7 @@
         }
     } error:NULL];
     [UIView aspect_hookSelector:@selector(pointInside:withEvent:) withOptions:AspectPositionInstead usingBlock:^(id<AspectInfo> aspectInfo, CGPoint point ,UIEvent *event) {
-        NSLog(@"pointInside %@", aspectInfo.instance);
+        [self log:aspectInfo];
         // Call original implementation.
         BOOL isPointInside;
         NSInvocation *invocation = aspectInfo.originalInvocation;
@@ -34,12 +34,70 @@
         [invocation getReturnValue:&isPointInside];
         if (isPointInside) {
             if([NSStringFromClass(UIWindow.class) isEqualToString: NSStringFromClass([aspectInfo.instance class])]){
-                pointInsideChain = [[NSMutableString alloc] initWithFormat:@"%@ %p",NSStringFromClass(self.class),self];
+                pointInsideChain = [[NSMutableString alloc] initWithFormat:@"%@ %p",NSStringFromClass([aspectInfo.instance class]),aspectInfo.instance];
             }
             else {
-                [pointInsideChain appendFormat:@" => %@ %p",NSStringFromClass(self.class),self];
+                [pointInsideChain appendFormat:@" => %@ %p",NSStringFromClass([aspectInfo.instance class]),aspectInfo.instance];
             }
         }
     } error:NULL];
+    
+    [UIResponder aspect_hookSelector:@selector(touchesBegan:withEvent:) withOptions:AspectPositionAfter usingBlock:^(id<AspectInfo> aspectInfo, NSSet<UITouch *> *touches, UIEvent *event) {
+        [self log:aspectInfo];
+    } error:NULL];
+    [UIResponder aspect_hookSelector:@selector(touchesMoved:withEvent:) withOptions:AspectPositionAfter usingBlock:^(id<AspectInfo> aspectInfo, NSSet<UITouch *> *touches, UIEvent *event) {
+        [self log:aspectInfo];
+    } error:NULL];
+    [UIResponder aspect_hookSelector:@selector(touchesEnded:withEvent:) withOptions:AspectPositionBefore usingBlock:^(id<AspectInfo> aspectInfo, NSSet<UITouch *> *touches, UIEvent *event) {
+        [self log:aspectInfo];
+    } error:NULL];
+    [UIResponder aspect_hookSelector:@selector(touchesCancelled:withEvent:) withOptions:AspectPositionAfter usingBlock:^(id<AspectInfo> aspectInfo, NSSet<UITouch *> *touches, UIEvent *event) {
+        [self log:aspectInfo];
+    } error:NULL];
+    
+    
+    
+    [UIGestureRecognizer aspect_hookSelector:@selector(touchesBegan:withEvent:) withOptions:AspectPositionAfter usingBlock:^(id<AspectInfo> aspectInfo, NSSet<UITouch *> *touches, UIEvent *event) {
+        [self log:aspectInfo];
+    } error:NULL];
+    [UIGestureRecognizer aspect_hookSelector:@selector(touchesMoved:withEvent:) withOptions:AspectPositionAfter usingBlock:^(id<AspectInfo> aspectInfo, NSSet<UITouch *> *touches, UIEvent *event) {
+        [self log:aspectInfo];
+    } error:NULL];
+    [UIGestureRecognizer aspect_hookSelector:@selector(touchesEnded:withEvent:) withOptions:AspectPositionAfter usingBlock:^(id<AspectInfo> aspectInfo, NSSet<UITouch *> *touches, UIEvent *event) {
+        [self log:aspectInfo];
+    } error:NULL];
+    [UIGestureRecognizer aspect_hookSelector:@selector(touchesCancelled:withEvent:) withOptions:AspectPositionAfter usingBlock:^(id<AspectInfo> aspectInfo, NSSet<UITouch *> *touches, UIEvent *event) {
+        [self log:aspectInfo];
+    } error:NULL];
+    
+    
+    [UIControl aspect_hookSelector:@selector(beginTrackingWithTouch:withEvent:) withOptions:AspectPositionAfter usingBlock:^(id<AspectInfo> aspectInfo, UITouch *touch, UIEvent *event) {
+        [self log:aspectInfo];
+    } error:NULL];
+    [UIControl aspect_hookSelector:@selector(continueTrackingWithTouch:withEvent:) withOptions:AspectPositionAfter usingBlock:^(id<AspectInfo> aspectInfo, UITouch *touch, UIEvent *event) {
+        [self log:aspectInfo];
+    } error:NULL];
+    [UIControl aspect_hookSelector:@selector(endTrackingWithTouch:withEvent:) withOptions:AspectPositionAfter usingBlock:^(id<AspectInfo> aspectInfo, UITouch *touch, UIEvent *event) {
+        [self log:aspectInfo];
+    } error:NULL];
+    [UIControl aspect_hookSelector:@selector(cancelTrackingWithEvent:) withOptions:AspectPositionAfter usingBlock:^(id<AspectInfo> aspectInfo,UIEvent *event) {
+        [self log:aspectInfo];
+    } error:NULL];
+    
+    
+    [UIApplication aspect_hookSelector:@selector(sendEvent:) withOptions:AspectPositionAfter usingBlock:^(id<AspectInfo> aspectInfo,UIEvent *event) {
+        [self log:aspectInfo];
+        UITouch *touch = [event.allTouches anyObject];
+        NSLog(@"UIApplication sendEvent:%@,%@", touch.window,touch.view);
+    } error:NULL];
+    
+    [UIWindow aspect_hookSelector:@selector(sendEvent:) withOptions:AspectPositionAfter usingBlock:^(id<AspectInfo> aspectInfo,UIEvent *event) {
+        UITouch *touch = [event.allTouches anyObject];
+        NSLog(@"UIWindow sendEvent:%@,%@", touch.window,touch.view);
+    } error:NULL];
+}
+
++ (void)log:(id<AspectInfo>)aspectInfo {
+    NSLog(@"class:%@;selector:%@;address:%p",NSStringFromClass([aspectInfo.instance class]),NSStringFromSelector(aspectInfo.originalInvocation.selector),aspectInfo.instance);
 }
 @end

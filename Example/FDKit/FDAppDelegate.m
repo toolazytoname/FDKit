dd205b4e237992b6602492611ccbe6a73dcba29f
weichao <weichao@yiche.com>
2019-02-21 21:57:13 +0800

    增加随机数获取方法

--- Example/FDKit/FDAppDelegate.m
+++ Example/FDKit/FDAppDelegate.m
@@ -7,12 +7,18 @@
 //
 
 #import "FDAppDelegate.h"
+#import "NSString+FDAdd.h"
 
 @implementation FDAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+//    NSString *test = [NSString fd_randomWithLength:6];
+//    NSLog(@"%@",test);
+    NSUInteger powInteger = pow(10, 4);
+
+
     return YES;
 }
 
@@ -20,6 +26,7 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application

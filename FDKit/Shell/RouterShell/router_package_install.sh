458159d478d623fad072d829ae1c3f93e1ff50e0
weichao <weichao@yiche.com>
2019-10-22 14:05:11 +0800

    edit shell

--- FDKit/Shell/RouterShell/router_package_install.sh
+++ FDKit/Shell/RouterShell/router_package_install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #example ./
-
+# 66
+# ./router_package_install.sh shadowsocks https://github.com/toolazytoname/FDKit/raw/master/FDKit/Shell/RouterShell/package/66/shadowsocks.tar.gz
+# 87
 # ./router_package_install.sh koolproxy  https://github.com/toolazytoname/FDKit/raw/master/FDKit/Shell/RouterShell/package/87/koolproxy.tar.gz
 # ./router_package_install.sh shadowsocks https://github.com/toolazytoname/FDKit/raw/master/FDKit/Shell/RouterShell/package/87/shadowsocks_4.1.7.tar.gz
 
